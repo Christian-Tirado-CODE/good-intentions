97381ff7d608757846a1f1ab887e6d9adc0e4bb0
Christian-Tirado-CODE <tiradoc02@gmail.com>
2020-11-14 11:15:05 -0400

    insignificant changes

--- src/components/SearchNonProfits/CategoryOptions/CategoryCard/CategoryCard.jsx
+++ src/components/SearchNonProfits/CategoryOptions/CategoryCard/CategoryCard.jsx
@@ -4,10 +4,12 @@ import React from 'react';
 const categoryCard = (props) => {
     return (
         <div className={classes.CategoryCard}>
-            {/* IMAGE, ORG.NAME, DESCRIPTION*/}
-            <img src={require("../../../../img/" + props.categoryCardImage + ".jpg").default} className={classes.CategoryCardImage}/> 
+           
+           <img src={require("../../../../img/" + props.categoryCardImage + ".jpg").default} className={classes.CategoryCardImage}/>
+           <div className={classes.CategoryCardTextContainer}>
            <h2 className={classes.CategoryCardTitle}>{props.categoryCardTitle}</h2>
            <p className={classes.CategoryCardDescription}>{props.categoryCardDescription}</p>
+           </div>
         </div>
     );
 };
